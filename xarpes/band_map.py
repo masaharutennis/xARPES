366ef732b8147f9d2a61d097f120cb9f9e48d46b
tvanwaas <>
2024-05-19 21:58:55 +0200

    changed mu to hnuminphi

--- xarpes/band_map.py
+++ xarpes/band_map.py
@@ -20,29 +20,29 @@ class band_map():
         Temperature of the band map in Kelvin.
     """
     
-    def __init__(self, intensities, angles, ekin, energy_resolution=None, temperature=None, mu=None):
+    def __init__(self, intensities, angles, ekin, energy_resolution=None, temperature=None, hnuminphi=None):
         self.intensities=intensities
         self.angles=angles
         self.ekin=ekin
         self.energy_resolution=energy_resolution
         self.temperature=temperature
-        self.mu=mu
+        self.hnuminphi=hnuminphi
 
 
     @property
-    def mu(self):
+    def hnuminphi(self):
         """
         Returns the chemical potential corresponding to a particular kinetic energy.
         """
-        return self._mu
+        return self._hnuminphi
 
     
-    @mu.setter
-    def mu(self, mu):
+    @hnuminphi.setter
+    def hnuminphi(self, hnuminphi):
         """
         Sets the chemical potential corresponding to a particular kinetic energy.
         """
-        self._mu=mu
+        self._hnuminphi=hnuminphi
 
 
     
@@ -55,7 +55,7 @@ class band_map():
            
     
     @add_fig_kwargs
-    def fit_fermi_edge(self, mu_guess, background_guess=0, integrated_weight_guess=1, \
+    def fit_fermi_edge(self, hnuminphi_guess, background_guess=0, integrated_weight_guess=1, \
                        angle_min=-np.infty, angle_max=np.infty, ekin_min=-np.infty, ekin_max=np.infty, \
                         ax=None, **kwargs):
         """
@@ -65,7 +65,7 @@ class band_map():
         ----------
         Matplotlib-Figure
         """
-        from xarpes.functions import fit_leastsq
+        from xarpes.functions import fit_leastsq        
         
         ax, fig, plt = get_ax_fig_plt(ax=ax)
         
@@ -78,21 +78,20 @@ class band_map():
         energy_range = self.ekin[min_ekin_index:max_ekin_index]
         
         integrated_intensity = np.trapz(self.intensities[min_ekin_index:max_ekin_index, min_angle_index:max_angle_index], axis=1)
-
     
-        fdir_initial = fermi_dirac(temperature=self.temperature, mu=mu_guess, background=background_guess, \
+        fdir_initial = fermi_dirac(temperature=self.temperature, hnuminphi=hnuminphi_guess, background=background_guess, \
                                    integrated_weight=integrated_weight_guess, name="Initial guess")
 
-        parameters = np.array([mu_guess, background_guess, integrated_weight_guess])
+        parameters = np.array([hnuminphi_guess, background_guess, integrated_weight_guess])
         
         extra_args = (self.energy_resolution)
         
         popt, pcov = fit_leastsq(parameters, energy_range, integrated_intensity, fdir_initial, extra_args)
         
-        fdir_final = fermi_dirac(temperature=self.temperature, mu=popt[0], background=popt[1], integrated_weight=popt[2], \
+        fdir_final = fermi_dirac(temperature=self.temperature, hnuminphi=popt[0], background=popt[1], integrated_weight=popt[2], \
                                   name="Fitted result")
-
-        self.mu = popt[0]
+        
+        self.hnuminphi = popt[0]
 
         ax.set_xlabel(r"$E_{\mathrm{kin}}$ (-)")
         ax.set_ylabel("Counts (-)")
--- xarpes/distributions.py
+++ xarpes/distributions.py
@@ -1,7 +1,6 @@
 # To do: Convert the Fermi-Dirac distribution to a proper distribution
 # Convolve will be a method for the distributions, whether it be an energy or momentum convolution.
 
-import numpy as np
 
 class distribution:
     """
@@ -9,9 +8,24 @@ class distribution:
     """
     def __init__(self, name):
         self.name = name
+        
+    def dist(self):
+        return self._dist
 
 
-class fermi_dirac(distribution):
+class unique_distribution(distribution):
+    """
+    TBD
+    """
+    def __init__(self, name):
+        super().__init__(name)
+        self._label = name
+
+    def label(self):
+        return self._label
+
+
+class fermi_dirac(unique_distribution):
     """
     Returns the Fermi-Dirac distribution. 
 
@@ -27,50 +41,95 @@ class fermi_dirac(distribution):
     
     """
     
-    def __init__(self, temperature, mu, background=0, integrated_weight=1, name="fermi_dirac"):
+    def __init__(self, temperature, hnuminphi, background=0, integrated_weight=1, name="fermi_dirac"):
         super().__init__(name)
         self.temperature=temperature
-        self.mu=mu
+        self.hnuminphi=hnuminphi
         self.background=background
         self.integrated_weight=integrated_weight
         self.name=name
 
 
-    def __call__(self, energy_range, mu, background, integrated_weight, energy_resolution):
+    def __call__(self, energy_range, hnuminphi, background, integrated_weight, energy_resolution):
         """
         TBD
         """
         from scipy.ndimage import gaussian_filter
+        import numpy as np
+        sigma_extend = 5                   # Extend data range by "5 sigma"
         fwhm_to_std = np.sqrt(8*np.log(2)) # Conversion from FWHM to standard deviation [-]
         k_B = 8.617e-5                     # Boltzmann constant [eV/K]
         k_BT = self.temperature*k_B
         step_size=np.abs(energy_range[1]-energy_range[0])
-        result = integrated_weight/(1+np.exp((energy_range-mu)/k_BT))+background
-        return gaussian_filter(result, sigma=energy_resolution/(fwhm_to_std*step_size))
+        estep = energy_resolution/(step_size*fwhm_to_std)
+        enumb = int(sigma_extend*estep)
+        extend = np.linspace(energy_range[0]-enumb*step_size, energy_range[-1]+enumb*step_size, \
+                             len(energy_range)+2*enumb)
+        result = integrated_weight/(1+np.exp((extend-hnuminphi)/k_BT))+background
+        return gaussian_filter(result, sigma=estep)[enumb:-enumb]
 
     
-    def calculate(self, energy_range):
+    def evaluate(self, energy_range):
         """
-        Calculates the Fermi-Dirac distribution on a certain energy range
+        Evaluates the Fermi-Dirac distribution on a certain energy range.
         """
+        import numpy as np
         k_B = 8.617e-5 # Boltzmann constant [eV/K]
         k_BT = self.temperature*k_B
-        return self.integrated_weight/(1+np.exp((energy_range-self.mu)/k_BT))+self.background
+        return self.integrated_weight/(1+np.exp((energy_range-self.hnuminphi)/k_BT))+self.background
 
 
     def convolve(self, energy_range, energy_resolution):
         """
         TBD
         """
+        import numpy as np
         from scipy.ndimage import gaussian_filter
-        step_size=np.abs(energy_range[1]-energy_range[0])
+        sigma_extend = 5                   # Extend data range by "5 sigma"
         fwhm_to_std = np.sqrt(8*np.log(2)) # Conversion from FWHM to standard deviation [-]
-        return gaussian_filter(self.calculate(energy_range), sigma=energy_resolution/(fwhm_to_std*step_size))
-        
+        step_size=np.abs(energy_range[1]-energy_range[0])
+        estep = energy_resolution/(step_size*fwhm_to_std)
+        enumb = int(sigma_extend*estep)
+        extend = np.linspace(energy_range[0]-enumb*step_size, energy_range[-1] + enumb*step_size, \
+                             len(energy_range)+2*enumb)
+        return gaussian_filter(self.evaluate(extend), sigma=estep)[enumb:-enumb]
 
 
+class constant(unique_distribution):
+    """
+    TBD
+    """
+    def __init__(self, offset):
+        super().__init__(name="constant")
+        self._offset = offset
+
+    def offset(self):
+        return self._offset
+    
+    def set_offset(self, x):
+        self._offset = x
 
 
+class linear(unique_distribution):
+    """
+    TBD
+    """
+    def __init__(self, slope, offset):
+        super().__init__(name="linear")
+        self._offset = offset
+        self._slope = slope
+
+    def offset(self):
+        return self._offset
+    
+    def set_offset(self, x):
+        self._offset = x
+    
+    def slope(self):
+        return self._slope
+    
+    def set_slope(self, x):
+        self._slope = x
 
 
 
