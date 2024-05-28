9cfd16a7b835ea25c49e876021fad16efe00373d
Jan Berges <jan.berges@uni-bremen.de>
2024-05-28 12:46:22 +0200

    Generate documentation in HTML using Sphinx

---
+++ doc/conf.py
@@ -0,0 +1,24 @@
+# Configuration file for the Sphinx documentation builder.
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+project = 'xARPES'
+copyright = '2024 xARPES Developers'
+
+html_logo = '../logo/xarpes_small.svg'
+html_theme_options = {'logo_only': True}
+
+extensions = ['sphinx.ext.autodoc', 'numpydoc', 'm2r2']
+
+exclude_patterns = ['README.md']
+
+mathjax_path = ('https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js'
+    '?config=TeX-AMS-MML_HTMLorMML')
+
+rst_epilog = '''
+.. include:: <isogrk1.txt>
+'''
+
+html_theme = 'sphinx_rtd_theme'
+
+numpydoc_show_class_members = False
---
+++ doc/index.rst
@@ -0,0 +1,19 @@
+.. highlight:: bash
+.. mdinclude:: ../README.md
+
+Modules
+-------
+
+.. toctree::
+   :maxdepth: 1
+
+   modules/band_map
+   modules/distributions
+   modules/functions
+   modules/plotting
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`search`
